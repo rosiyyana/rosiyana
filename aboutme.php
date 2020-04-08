973eeb649046ff801d39e12b9ddad63f5bb56a1a
rosiyyana <63337889+rosiyyana@users.noreply.github.com>
2020-04-08 14:34:35 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,9 @@
+<?php 
+    echo "Nama    : Rosiyana";
+    echo "<br>";
+    echo "Kelas   : XI RPL 2";
+    echo "<br>";
+    echo "Sekolah : SMK TarunaBhakti Depok";
+    echo "<br>";
+    echo "Alamat  : Jl.Masjid Al-islah Cisalak Rt/Rw 01/08";
+?>
